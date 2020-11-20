aa90c9af9370eae17e582d76148957fbdbbb6cc8
Chad Minick <cminick@verizonmedia.com>
2020-11-20 14:57:52 -0600

    chore: update vulnerable packages
    - Bringing in a version of nodemailer that has a high level remove 
    execution vulnerability.
    - Noticed and fixed other vulnerable packages
    - Caused an update in linting/prettier packages so had to make some 
    style updates

--- test/test.js
+++ test/test.js
@@ -8,11 +8,11 @@ const previewEmail = require('../lib');
 
 const transport = nodemailer.createTransport({ jsonTransport: true });
 
-test('returns function', t => {
+test('returns function', (t) => {
   t.true(typeof previewEmail === 'function');
 });
 
-test('opens a preview email', async t => {
+test('opens a preview email', async (t) => {
   const message = {
     from: 'niftylettuce <niftylettuce+from@gmail.com>',
     to: 'niftylettuce+to@gmail.com, niftylettuce <niftylettuce+test@gmail.com>',
@@ -37,38 +37,38 @@ test('opens a preview email', async t => {
       unsubscribe: 'https://niftylettuce.com/unsubscribe'
     }
   };
-  const res = await transport.sendMail(message);
-  const url = await previewEmail(JSON.parse(res.message));
+  const result = await transport.sendMail(message);
+  const url = await previewEmail(JSON.parse(result.message));
   t.true(typeof url === 'string');
 });
 
-test('does not open', async t => {
+test('does not open', async (t) => {
   const url = await previewEmail({}, { open: false });
   t.true(typeof url === 'string');
 });
 
-test('invalid message', async t => {
+test('invalid message', async (t) => {
   const error = await t.throwsAsync(previewEmail(false));
   t.is(error.message, 'Message argument is required');
 });
 
-test('custom id', async t => {
+test('custom id', async (t) => {
   const id = new Date().getTime().toString();
   const url = await previewEmail({}, { id, open: false });
   t.is(path.basename(url).replace('.html', ''), id);
 });
 
-test('does not open in browser', async t => {
+test('does not open in browser', async (t) => {
   const url = await previewEmail({}, { open: false });
   t.true(typeof url === 'string');
 });
 
-test('transform URL', async t => {
+test('transform URL', async (t) => {
   const url = await previewEmail(
     {},
     {
       open: false,
-      urlTransform: path => `http://localhost:8000/${path}`
+      urlTransform: (path) => `http://localhost:8000/${path}`
     }
   );
   t.regex(url, /^http:\/\/localhost:8000\//);
